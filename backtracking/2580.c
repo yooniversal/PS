2687b7747e20d0d00e813379fffc118cccad6a6a
yooniversal <61930524+yooniversal@users.noreply.github.com>
2020-05-29 20:34:19 +0900

    baekjoon 05.29.
    not complete solution
    lack of experience related with recursive, backtracking
    This code need to be modifed.

--- backtracking/2580.c
+++ backtracking/2580.c
@@ -1,4 +1,5 @@
 //9663
+//non-completed
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,12 +25,14 @@ int isSameValue(int arr[][9]) {
 			//row check
 			for (i = 0; i < 9; i++)
 				for (j = i + 1; j < 9; j++)
+					if (arr[row][i] != 0)
 					if (arr[row][i] == arr[row][j])
-						return 1;
+							return 1;
 
 			//col check
 			for (i = 0; i < 9; i++)
 				for (j = i + 1; j < 9; j++)
+					if (arr[i][col] != 0)
 					if (arr[i][col] == arr[j][col])
 						return 1;
 
@@ -38,6 +41,7 @@ int isSameValue(int arr[][9]) {
 				for (i = 0; i < 3; i++)
 					for (j = 0; j < 3; j++)
 						if (arr[row][col] == arr[i][j])
+							if (arr[i][j] != 0)
 							if (!(row == i && col == j))
 								return 1;
 			}
@@ -45,6 +49,7 @@ int isSameValue(int arr[][9]) {
 				for (i = 0; i < 3; i++)
 					for (j = 3; j < 6; j++)
 						if (arr[row][col] == arr[i][j])
+							if (arr[i][j] != 0)
 							if (!(row == i && col == j))
 								return 1;
 			}
@@ -52,6 +57,7 @@ int isSameValue(int arr[][9]) {
 				for (i = 0; i < 3; i++)
 					for (j = 6; j < 9; j++)
 						if (arr[row][col] == arr[i][j])
+							if (arr[i][j] != 0)
 							if (!(row == i && col == j))
 								return 1;
 			}
@@ -59,6 +65,7 @@ int isSameValue(int arr[][9]) {
 				for (i = 3; i < 6; i++)
 					for (j = 0; j < 3; j++)
 						if (arr[row][col] == arr[i][j])
+							if (arr[i][j] != 0)
 							if (!(row == i && col == j))
 								return 1;
 			}
@@ -66,6 +73,7 @@ int isSameValue(int arr[][9]) {
 				for (i = 3; i < 6; i++)
 					for (j = 3; j < 6; j++)
 						if (arr[row][col] == arr[i][j])
+							if (arr[i][j] != 0)
 							if (!(row == i && col == j))
 								return 1;
 			}
@@ -73,6 +81,7 @@ int isSameValue(int arr[][9]) {
 				for (i = 3; i < 6; i++)
 					for (j = 6; j < 9; j++)
 						if (arr[row][col] == arr[i][j])
+							if (arr[i][j] != 0)
 							if (!(row == i && col == j))
 								return 1;
 			}
@@ -80,6 +89,7 @@ int isSameValue(int arr[][9]) {
 				for (i = 6; i < 9; i++)
 					for (j = 0; j < 3; j++)
 						if (arr[row][col] == arr[i][j])
+							if (arr[i][j] != 0)
 							if (!(row == i && col == j))
 								return 1;
 			}
@@ -87,6 +97,7 @@ int isSameValue(int arr[][9]) {
 				for (i = 6; i < 9; i++)
 					for (j = 3; j < 6; j++)
 						if (arr[row][col] == arr[i][j])
+							if (arr[i][j] != 0)
 							if (!(row == i && col == j))
 								return 1;
 			}
@@ -94,42 +105,71 @@ int isSameValue(int arr[][9]) {
 				for (i = 6; i < 9; i++)
 					for (j = 6; j < 9; j++)
 						if (arr[row][col] == arr[i][j])
+							if (arr[i][j] != 0)
 							if (!(row == i && col == j))
-								return 1;
+								return 1;			
 			}
 		}
+
+	//if (!hasnotzero(arr))
+	//	return 1;
 
 	return 0;
 }
 
 void fill(int arr[][9], int row, int col) {
 
-	int a, b;
+	int a, b, temp;
 	
 	subtemp = row*9+col;
 
 	if (subtemp < 81) {
-		if (!isSameValue(arr)) {
+		if (hasnotzero(arr)) {
+			if (isSameValue(arr)) {
+				for (a = 0; a < 9; a++) {
+					for (b = 0; b < 9; b++)
+						printf("%d ", arr[a][b]);
+					printf("\n");
+				}
+				exit(1);
+			}
+		}
+		else {
+			a = 1;
+			while(1){
+				//if(a==1)
+				//	temp = arr[row][col];
+				if (arr[row][col] == a - 1)
+					arr[row][col] = a;
+				//pruning
+				if (isSameValue(arr))
+					break;
+				
+				//112345 이런 경우 다음으로 넘어가기 [pruning]
+				//if (!(row == 0 && col == 0))
+				//	if (arr[(subtemp - 1) / 9][(subtemp - 1) % 9] == arr[row][col])
+				//		break;
+
+				subtemp++;
+				fill(arr, subtemp / 9, subtemp % 9);
+
+				if (a < 9)
+					a++;
+				else
+					a = 1;
+			}
+			//if (isSameValue(arr))
+			//	arr[row][col] = temp;
+
+			//test print
 			for (a = 0; a < 9; a++) {
 				for (b = 0; b < 9; b++)
 					printf("%d ", arr[a][b]);
 				printf("\n");
 			}
 			printf("\n");
-			return;
-		}
-		else {
-			if (arr[row][col] == 0) {
-				for (a = 1; a <= 9; a++) {
-					arr[row][col] = a;
-					subtemp++;
-					fill(arr, subtemp/9, subtemp%9);
-				}
-			}
 		}
 	}
-
-	
 }
 
 int main() {
@@ -142,6 +182,7 @@ int main() {
 			scanf("%d", &arr[i][j]);
 
 	fill(arr, 0, 0);
+	//printf("%d", isSameValue(arr));
 
 	return 0;
 }
