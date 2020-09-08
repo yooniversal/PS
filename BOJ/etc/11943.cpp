7f651c5e053274663eca661d7e9256c73b7078a8
yooniversal <61930524+yooniversal@users.noreply.github.com>
2020-09-09 00:04:55 +0900

    09.08.

---
+++ BOJ/etc/11943.cpp
@@ -0,0 +1,34 @@
+//11943
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <vector>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <algorithm>
+#include <map>
+using namespace std;
+#define INF 1000000001
+#define MAX 20001
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<pair<int, int>> vii;
+typedef pair<int, int> ii;
+
+int main() {
+
+	cin.tie(nullptr);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int a, b, c, d; cin >> a >> b >> c >> d;
+
+	cout << min(a + d, b + c);
+
+	return 0;
+}
