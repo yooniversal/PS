d61ade15970c45d0d61a18244f4f11c6b4dd72e6
yooniversal <61930524+yooniversal@users.noreply.github.com>
2020-11-07 01:24:48 +0900

    새 풀이로 업데이트
    시간 제한 조정으로 TLE 받아서 새로  푼걸로 업로드 - O(NlogN)

--- BOJ/SegmentTree/1168.cpp
+++ BOJ/SegmentTree/1168.cpp
@@ -23,35 +23,20 @@ private:
         return tree[node] = leftValue + rightValue;
     }
 
-    ll _find(int start, int end, int node, int left, int right) {
-        if (left > end || right < start) return 0;
-        if (left <= start && end <= right) return tree[node];
+    ll _query(int node, int start, int end, int idx) {
+        if (start == end) return start;
         int mid = (start + end) / 2;
-        return _find(start, mid, 2 * node, left, right) + _find(mid + 1, end, 2 * node + 1, left, right);
+        if (idx <= tree[node * 2]) return _query(node * 2, start, mid, idx);
+        return _query(node * 2 + 1, mid + 1, end, idx - tree[node * 2]);
     }
 
 public:
     SumSeg() { tree.resize(MAX * 4, 0); }
-    ll update(int idx, ll val) { return update(0, MAX - 1, 1, idx, val); }
-    ll find(int l, int r) { return _find(0, MAX - 1, 1, l, r); }
+    ll update(int idx, ll val) { return update(1, MAX, 1, idx, val); }
+    ll query(int k) { return _query(1, 1, MAX, k); } //k번째 값 호출
 };
 
 int n, m;
-
-ll binary_search(SumSeg& Seg, ll l, ll r, ll front) {
-    ll ret = 1;
-    while (l <= r) {
-        ll m = (l + r) / 2, tmp = Seg.find(1, m);
-        if (tmp < front) {
-            l = m + 1;
-        }
-        else {
-            r = m - 1;
-            ret = m;
-        }
-    }
-    return ret;
-}
 
 int main() {
 
@@ -64,13 +49,13 @@ int main() {
     for (int i = 1; i <= n; ++i)
         Seg.update(i, 1);
     
-    ll front = 1; //현재 위치를 포함한 앞에 있는 갯수
-    vector<ll> ret(n, 0);
     int cnt = 0;
-    for(int SIZE=n; SIZE>0; --SIZE) {
+    vector<ll> ret(n, 0);
+    ll front = 1;
+    for (int SIZE = n; SIZE > 0; --SIZE) {
         front += m - 1;
         front = front % SIZE ? front % SIZE : SIZE;
-        ll idx = binary_search(Seg, 1, n, front);
+        ll idx = Seg.query(front);
         Seg.update(idx, 0);
         ret[cnt++] = idx;
     }
