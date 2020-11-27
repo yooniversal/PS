0bf5a06492129af4e77ad89f214524683603edef
yooniversal <iteratively@naver.com>
2020-11-28 01:43:37 +0900

    구간합을 이용한 풀이 추가
    구간합만 이용한 풀이 뿐만 아니라
    구간합을 응용한 DP 풀이로 O(n)만에 답을 구할 수 있음.

--- BOJ/dp/10211.cpp
+++ BOJ/dp/10211.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 #define INF 1000000001
 #define MAX 100005
+#define MOD 1000000007
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
@@ -18,7 +19,8 @@ int main() {
 	int t; cin >> t;
 	while (t--) {
 		int n; cin >> n;
-		vi a(n), pSum(n + 1, 0);
+		//O(n^2)
+		/*vi a(n), pSum(n + 1, 0);
 		for (int i = 0; i < n; ++i) {
 			cin >> a[i];
 			pSum[i+1] = pSum[i] + a[i];
@@ -28,6 +30,16 @@ int main() {
 		for (int i = 0; i < n; ++i)
 			for (int j = i+1; j <= n; ++j)
 				ret = max(ret, pSum[j] - pSum[i]);
+		cout << ret << '\n';*/
+
+		//O(n)
+		int pSum1 = 0, pSum2 = 0, ret = -INF;
+		for (int i = 0; i < n; ++i) {
+			int x; cin >> x;
+			pSum1 += x;
+			ret = max(ret, pSum1 - pSum2);
+			pSum2 = min(pSum1, pSum2);
+		}
 		cout << ret << '\n';
 	}
 
