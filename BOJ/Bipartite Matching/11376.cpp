53b78f00ff256b43da40b1169764e115e5c88f9f
yooniversal <iteratively@naver.com>
2020-12-14 00:03:36 +0900

    맞았습니다!!
    재채점받고 틀렸습니다떠서 재제출 후 AC

--- BOJ/Bipartite Matching/11376.cpp
+++ BOJ/Bipartite Matching/11376.cpp
@@ -1,65 +1,53 @@
 //11376
-
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <cstdio>
-#include <vector>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <cmath>
-#include <algorithm>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
-#define INF 987654321
-#define MAX 1001
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
 typedef vector<pair<int, int>> vii;
 typedef pair<int, int> ii;
+const int MAX = 1005, INF = 1000000001, MOD = 1234567;
 
 vi person[MAX];
 bool chk[MAX];
 int from[MAX];
 
 bool dfs(int cur) {
-    for (auto& next : person[cur]) {
-        if (chk[next]) continue;
-        chk[next] = true;
+	for (auto& next : person[cur]) {
+		if (chk[next]) continue;
+		chk[next] = true;
 
-        if (from[next] == 0 || dfs(from[next])) {
-            from[next] = cur;
-            return true;
-        }
-    }
-    return false;
+		if (from[next] == 0 || dfs(from[next])) {
+			from[next] = cur;
+			return true;
+		}
+	}
+	return false;
 }
 
 int main() {
 
-    cin.tie(nullptr);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
 
-    int n, m; cin >> n >> m;
-    for (int i = 1; i <= n; i++) {
-        int work; cin >> work;
-        for (int j = 0; j < work; j++) {
-            int next; cin >> next;
-            person[i].push_back(next);
-        }
-    }
+	int n, m; cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		int work; cin >> work;
+		for (int j = 0; j < work; j++) {
+			int next; cin >> next;
+			person[i].push_back(next);
+		}
+	}
 
-    int cnt = 0;
-    for (int i = 1; i <= n; i++) {
-        fill(chk, chk + MAX, false);
-        if (dfs(i)) cnt++;
-        if (dfs(i)) cnt++;
-    }
+	int cnt = 0;
+	for (int i = 1; i <= n; i++) {
+		memset(chk, 0, sizeof(chk));
+		if (dfs(i)) cnt++;
+		memset(chk, 0, sizeof(chk));
+		if (dfs(i)) cnt++;
+	}
+	cout << cnt << '\n';
 
-    cout << cnt;
-
-    return 0;
+	return 0;
 }
