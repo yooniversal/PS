f7d4abe896b3b7ecf5e526fc4695d63b9574cc35
yooniversal <iteratively@naver.com>
2021-01-10 18:53:17 +0900

    맞았습니다!!
    틀렸습니다 -> 맞았습니다!!

--- BOJ/dfs, bfs/9376.cpp
+++ BOJ/dfs, bfs/9376.cpp
@@ -1,13 +1,12 @@
 //9376
 #include <bits/stdc++.h>
 using namespace std;
-#define INF 1000000000
-#define MAX 4000005
 typedef long long ll;
 typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
 typedef vector<pair<int, int>> vii;
 typedef pair<int, int> ii;
+const int MAX = 100001, INF = 1000000001, MOD = 1234567;
 
 struct P {
 	int x, y;
@@ -25,14 +24,14 @@ bool OOB(int x, int y) {
 }
 
 void bfs() {
-	person.push_back({0,0});
+	person.push_back({ 0,0 });
 	for (int turn = 0; turn < 3; ++turn) {
 		int cx = person.back().x, cy = person.back().y; person.pop_back();
 		deque<P> q;
 		q.push_back({ cx, cy });
 		while (!q.empty()) {
 			int x = q.front().x, y = q.front().y; q.pop_front();
-			if(d[x][y][turn] == -1) d[x][y][turn] = 0;
+			if (d[x][y][turn] == -1) d[x][y][turn] = 0;
 			for (int i = 0; i < 4; ++i) {
 				int nx = x + dx[i], ny = y + dy[i];
 				if (OOB(nx, ny)) continue;
@@ -63,22 +62,23 @@ int main() {
 		memset(d, -1, sizeof(d));
 
 		cin >> h >> w;
-		for (int i = 0; i < h+2; ++i) {
+		for (int i = 0; i < h + 2; ++i) {
 			if (i == 0 || i == h + 1) continue;
 			string s; cin >> s;
 			for (int j = 0; j < w; ++j) {
-				if (s[j] == '*') board[i][j+1] = -1;
-				else if (s[j] == '#') board[i][j+1] = 1;
-				else if (s[j] == '$') person.push_back({ i,j+1 });
+				if (s[j] == '*') board[i][j + 1] = -1;
+				else if (s[j] == '#') board[i][j + 1] = 1;
+				else if (s[j] == '$') person.push_back({ i,j + 1 });
 			}
 		}
 
 		bfs();
 
 		int ret = INF;
-		for(int i=0; i<h+2; ++i)
+		for (int i = 0; i < h + 2; ++i)
 			for (int j = 0; j < w + 2; ++j) {
 				int dist = d[i][j][0] + d[i][j][1] + d[i][j][2];
+				if (d[i][j][0] == -1 || d[i][j][1] == -1 || d[i][j][2] == -1) continue;
 				if (board[i][j] == -1) continue;
 				if (board[i][j] == 1) dist -= 2;
 				ret = min(ret, dist);
