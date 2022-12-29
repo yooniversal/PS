ece33814404f4ae7698609e9e6eaacd4c9da5523
yooniversal <iteratively@naver.com>
2022-12-30 00:57:01 +0900

    대표값2

---
+++ BOJ/sort/2587.cpp
@@ -0,0 +1,29 @@
+// 2587
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<pair<int, int>> vii;
+typedef pair<int, int> ii;
+const int MAX = 100005, INF = 1000000001, MOD = 9901;
+
+int main()
+{
+    cin.tie(nullptr);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int sum = 0;
+    vi a(5);
+    for ( int i = 0; i < 5; ++i )
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
+
+    sort(a.begin(), a.end());
+    cout << sum / 5 << '\n' << a[2] << '\n';
+
+    return 0;
+}
