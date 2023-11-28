c0005f4b013c1f6763c342a7b19b18aed88a47a8
yooniversal <iteratively@naver.com>
2023-11-28 21:08:55 +0900

    꼬마 정민

---
+++ BOJ/math/11382.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+
+    cin.tie(nullptr);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    ll a, b, c; cin >> a >> b >> c;
+    cout << a + b + c << '\n';
+
+    return 0;
+}
