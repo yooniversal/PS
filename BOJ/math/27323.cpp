0b4362340c41431cf9beb78968b7e585304d4872
yooniversal <iteratively@naver.com>
2023-12-15 02:01:38 +0900

    직사각형

---
+++ BOJ/math/27323.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+
+    cin.tie(nullptr);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    int a, b; cin >> a >> b;
+    cout << a*b << '\n';
+
+    return 0;
+}
