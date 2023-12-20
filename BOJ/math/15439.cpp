dc78d913e642e17c6a22267d4b7f18b14c8e3bc6
yooniversal <iteratively@naver.com>
2023-12-21 01:38:32 +0900

    베라의 패션

---
+++ BOJ/math/15439.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+
+const int INF = 987654321;
+
+int main() {
+
+    cin.tie(nullptr);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+    int n; cin >> n;
+    cout << n * (n-1) << '\n';
+
+    return 0;
+}
