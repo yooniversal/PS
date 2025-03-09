eb6093185ad64d8462a1c37d1f58b9dfb397b848
yooniversal <iteratively@naver.com>
2025-03-09 15:18:44 +0900

    Intersection of Two Arrays

---
+++ Leetcode/349.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    boolean[] chk = new boolean[1005];
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        List<Integer> ret = new ArrayList<>();
+
+        if (nums1.length >= nums2.length) {
+            getIntersectionArray(nums1, nums2, ret);
+        } else {
+            getIntersectionArray(nums2, nums1, ret);
+        }
+
+        return ret.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    void getIntersectionArray(int[] nums1, int[] nums2, List<Integer> ret) {
+        for (int i=0; i<nums1.length; i++) {
+            for (int j=0; j<nums2.length; j++) {
+                if (chk[nums2[j]] || nums1[i] != nums2[j]) continue;
+                chk[nums2[j]] = true;
+                ret.add(nums2[j]);
+                break;
+            }
+        }
+    }
+}
