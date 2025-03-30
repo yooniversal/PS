7f10eaae97671e68bfcae3460a6d84007784eb26
yooniversal <iteratively@naver.com>
2025-03-30 18:55:03 +0900

    Best Time to Buy and Sell Stock with Cooldown

---
+++ Leetcode/309.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    public:
+        const int INF = 987654321;
+            int cache[5005][2];
+        
+            int f(vector<int>& prices, int a, int b) {
+                if (a >= prices.size()) return 0;
+                int& ret = cache[a][b];
+                if (ret != -INF) return ret;
+                ret = 0;
+        
+                if (b == 1) {
+                    ret = max(ret, f(prices, a+2, 0) + prices[a]);
+                    ret = max(ret, f(prices, a+1, 1));
+                } else {
+                    ret = max(ret, f(prices, a+1, 0));
+                    ret = max(ret, f(prices, a+1, 1) - prices[a]);
+                }
+        
+                return ret;
+            }
+        
+            int maxProfit(vector<int>& prices) {
+                for (int i=0; i<5005; i++) {
+                    cache[i][0] = cache[i][1] = -INF;
+                }
+                return f(prices, 0, 0);
+            }
+    };
