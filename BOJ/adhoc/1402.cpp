97167f12a073d81041da6e163f9be53c05fe1f9e
yooniversal <iteratively@naver.com>
2025-08-31 13:57:08 +0900

    아무래도이문제는A번난이도인것같다

---
+++ BOJ/adhoc/1402.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+const int INF = 987654321;
+const int MOD = 1000000007;
+
+int main() {
+    FASTIO;
+
+    int t; cin >> t;
+    while (t--) {
+        int a, b; cin >> a >> b;
+        cout << "yes\n";
+    }
+
+    return 0;
+}
