6831f2837211251a774e039d07d54d514682f899
yooniversal <iteratively@naver.com>
2025-10-23 14:00:14 +0900

    약속

---
+++ BOJ/math/1183.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ii> vii;
+
+const int INF = 987654321;
+const int MOD = 1000000007;
+
+int main() {
+    FASTIO;
+
+    int n; cin >> n;
+    vi a(n), b(n), d(n);
+    for (int i=0; i<n; i++) {
+        cin >> a[i] >> b[i];
+        d[i] = a[i] - b[i];
+    }
+
+    if (n % 2 != 0) {
+        cout << 1 << '\n';
+    } else {
+        sort(d.begin(), d.end());
+        cout << d[d.size()/2] - d[d.size()/2 - 1] + 1 << '\n';
+    }
+
+    return 0;
+}
