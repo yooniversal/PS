2c5538a6217c556453f71c0340dfeb3729d9d7d7
yooniversal <iteratively@naver.com>
2026-01-05 21:50:44 +0900

    비트가 넘쳐흘러

---
+++ BOJ/math/17419.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FASTIO cin.tie(0); cout.tie(0); ios_base::sync_with_stdio(0);
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<ii> vii;
+const int INF = 987654321;
+
+int main() {
+    FASTIO;
+
+    int n; cin >> n;
+    string s; cin >> s;
+
+    int ret = 0;
+    for (int i=0; i<s.size(); i++) if (s[i] == '1') ret++;
+    cout << ret << '\n';
+
+    return 0;
+}
