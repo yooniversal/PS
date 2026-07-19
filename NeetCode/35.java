3120a91505593a9017f2cbd4ebcdb5813d918c04
yooniversal <iteratively@naver.com>
2026-07-19 17:59:59 +0900

    35

---
+++ NeetCode/35.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l=0, r=nums.length-1;
+        
+        int ret = nums.length;
+        while (l <= r) {
+            int m = (l+r)/2;
+
+            if (nums[m] == target) return m;
+            if (nums[m] < target) {
+                l = m+1;
+            } else {
+                ret = m;
+                r = m-1;
+            }
+        }
+
+        return ret;
+    }
+}
