16d7ebb9cbdb8f3fe0090fc418da2ad9ab776491
yooniversal <iteratively@naver.com>
2026-08-01 21:47:02 +0900

    11

---
+++ NeetCode/11.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int currentWidth = right - left;
+            int currentHeight = Math.min(height[left], height[right]);
+            int currentArea = currentWidth * currentHeight;
+            
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+}
